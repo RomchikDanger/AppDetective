3440fd8ce2e2fc6d57ee841b1fb418b95e260d63
RomchikDanger <the73112@gmail.com>
2026-05-20 00:47:40 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,30 +1,62 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, FileField, TextAreaField
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    FileField,
+    TextAreaField,
+)
 from wtforms.validators import DataRequired, Length
 
-# ==========ФОРМА РЕГИСТРАЦИИ============================================================
+
+# ========= ФОРМА РЕГИСТРАЦИИ =========
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=4)])
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=3)],
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=4)],
+    )
     submit = SubmitField('Register')
 
-# ==========ФОРМА ВХОДА==================================================================
+
+# ========= ФОРМА ВХОДА =========
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField(
+        'Username',
+        validators=[DataRequired()],
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Login')
 
-# ==========ФОРМА СОЗДАНИЯ ДЕЛА==========================================================
+
+# ========= ФОРМА СОЗДАНИЯ ДЕЛА =========
 class InvestigationForm(FlaskForm):
-    title = StringField('Название дела', validators=[DataRequired()])
+    title = StringField(
+        'Название дела',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Создать дело')
 
-# ==========ФОРМА ЗАГРУЗКИ УЛИКИ=========================================================
+
+# ========= ФОРМА ЗАГРУЗКИ УЛИКИ =========
 class EvidenceForm(FlaskForm):
-    photo = FileField('Фото-улика', validators=[DataRequired()])
+    photo = FileField(
+        'Фото-улика',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Загрузить улику')
 
-# ==========ФОРМА КОММЕНТАРИЯ============================================================
+
+# ========= ФОРМА КОММЕНТАРИЯ =========
 class CommentForm(FlaskForm):
-    text = TextAreaField('Комментарий', validators=[DataRequired()])
+    text = TextAreaField(
+        'Комментарий',
+        validators=[DataRequired()],
+    )
     submit = SubmitField('Отправить')
